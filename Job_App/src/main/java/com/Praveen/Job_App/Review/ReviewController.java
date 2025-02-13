00c39005f71578ca2249f78b7160f86453067a4f
PraveenRaamC <ckpraveenraam@gmail.com>
2025-02-13 11:46:12 +0530

    #3, Code for Model was finished

--- Job_App/src/main/java/com/Praveen/Job_App/Review/ReviewController.java
+++ Job_App/src/main/java/com/Praveen/Job_App/Review/ReviewController.java
@@ -1,9 +1,16 @@
 package com.Praveen.Job_App.Review;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/reviews")
 public class ReviewController {
+
+    @Autowired
+    ReviewService service;
+
+
+
 }
