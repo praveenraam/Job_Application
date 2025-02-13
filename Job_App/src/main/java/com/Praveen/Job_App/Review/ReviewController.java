1c12a8208b393ae429148878baf49648517081cc
PraveenRaamC <ckpraveenraam@gmail.com>
2025-02-13 11:43:00 +0530

    #3, Files required were created and added

---
+++ Job_App/src/main/java/com/Praveen/Job_App/Review/ReviewController.java
@@ -0,0 +1,9 @@
+package com.Praveen.Job_App.Review;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/reviews")
+public class ReviewController {
+}
