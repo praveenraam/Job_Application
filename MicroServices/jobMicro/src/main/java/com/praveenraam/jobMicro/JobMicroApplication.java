88573458d91af4391f1be65d1a28a12c68b5be59
PraveenRaamC <ckpraveenraam@gmail.com>
2025-02-17 23:39:00 +0530

    #4->#6, Project was init for Job

---
+++ MicroServices/jobMicro/src/main/java/com/praveenraam/jobMicro/JobMicroApplication.java
@@ -0,0 +1,13 @@
+package com.praveenraam.jobMicro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobMicroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobMicroApplication.class, args);
+	}
+
+}
---
+++ MicroServices/jobMicro/src/test/java/com/praveenraam/jobMicro/JobMicroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.praveenraam.jobMicro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobMicroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
