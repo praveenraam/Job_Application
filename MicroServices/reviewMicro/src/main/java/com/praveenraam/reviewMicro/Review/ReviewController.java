31f9049b980624cbdeebc14c92bb295f8cd5627c
PraveenRaamC <ckpraveenraam@gmail.com>
2025-02-19 17:34:59 +0530

    #4->#8, Review Modules were added

---
+++ MicroServices/reviewMicro/src/main/java/com/praveenraam/reviewMicro/Review/ReviewController.java
@@ -0,0 +1,48 @@
+package com.praveenraam.reviewMicro.Review;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/companies/{companyId}")
+public class ReviewController {
+
+    @Autowired
+    private ReviewService service;
+
+    @GetMapping("/reviews")
+    public ResponseEntity<List<Review>> getAllReviewsofCompany(@PathVariable Long companyId){
+        return new ResponseEntity<>(service.findAll(companyId), HttpStatus.OK);
+    }
+    @GetMapping("/reviews/{reviewId}")
+    public ResponseEntity<Review> getReviewById(@PathVariable Long companyId,@PathVariable Long reviewId){
+        Review review = service.findById(companyId,reviewId);
+
+        if(review != null) return new ResponseEntity<>(review,HttpStatus.OK);
+        return new ResponseEntity<>(null,HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping("/reviews")
+    public ResponseEntity<String> createReview(@PathVariable Long companyId, @RequestBody Review review){
+        return new ResponseEntity<>(service.createReview(companyId,review),HttpStatus.CREATED);
+    }
+
+    @PutMapping("/reviews/{reviewId}")
+    public ResponseEntity<Boolean> updateReviewById(@PathVariable Long reviewId,@PathVariable Long companyId,@RequestBody Review review){
+        if(service.UpdateById(reviewId,companyId,review))
+            return new ResponseEntity<>(true,HttpStatus.OK);
+        return new ResponseEntity<>(false,HttpStatus.NOT_FOUND);
+    }
+
+    @DeleteMapping("/reviews/{reviewId}")
+    public ResponseEntity<Boolean> deleteReviewById(@PathVariable Long reviewId){
+        if(service.deleteById(reviewId)){
+            return new ResponseEntity<>(true,HttpStatus.OK);
+        }
+        return new ResponseEntity<>(false,HttpStatus.NOT_ACCEPTABLE);
+    }
+}
---
+++ MicroServices/reviewMicro/src/main/java/com/praveenraam/reviewMicro/Review/ReviewService.java
@@ -0,0 +1,69 @@
+package com.praveenraam.reviewMicro.Review;
+
+import com.Praveen.Job_App.Company.Company;
+import com.Praveen.Job_App.Company.CompanyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
+public class ReviewService {
+
+    @Autowired
+    ReviewRepository repo;
+    @Autowired
+    CompanyService companyService;
+
+    public List<Review> findAll(Long companyId){
+        return repo.findByCompanyId(companyId);
+    }
+
+    public String createReview(Long companyId, Review review) {
+        Company company = companyService.findById(companyId);
+        if(company == null) return "Company not exists";
+
+        review.setCompany(company);
+        repo.save(review);
+
+        return "Review Added Successfully";
+    }
+
+    public Review findById(Long companyId, Long id) {
+
+        List<Review> reviewsOfCompany = this.findAll(companyId);
+
+        return reviewsOfCompany.stream()
+                .filter(review -> review.getId().equals(id))
+                .findFirst()
+                .orElse(null);
+    }
+
+    public boolean UpdateById(Long id,Long companyId,Review review){
+
+        Review updatingReview = this.findById(companyId,id);
+        if(updatingReview == null) return false;
+
+        updatingReview.setDescription(review.getDescription());
+        updatingReview.setRating(review.getRating());
+        updatingReview.setTitle(review.getTitle());
+
+        repo.save(updatingReview);
+        return true;
+    }
+
+    public boolean deleteById(Long Id){
+        if(!repo.existsById(Id)) return false;
+
+        try{
+            repo.deleteById(Id);
+            return true;
+        }
+        catch (Exception e){
+            return false;
+        }
+    }
+
+
+}
