16d292688158f2ac0067172e1155f7b3e0984df8
PraveenRaamC <ckpraveenraam@gmail.com>
2025-02-19 17:31:14 +0530

    #4->#8, Project init for Review Module

---
+++ MicroServices/reviewMicro/src/main/java/com/praveenraam/reviewMicro/ReviewMicroApplication.java
@@ -0,0 +1,13 @@
+package com.praveenraam.reviewMicro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewMicroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewMicroApplication.class, args);
+	}
+
+}
